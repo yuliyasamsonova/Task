40b27379a4548e823cf8f3c54ba9fd14104a4532
yuliyasamsonova <yuliyasamsonova01@gmail.com>
2019-06-08 01:14:10 +0300

    feat: add new functional (button-add img, button-print, button-download file)

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 
-let state = { };
+let state = {};
 
 document.querySelector(".save").addEventListener("click", function(){
-    // if (document.querySelector('input').value === '' && document.querySelector('input').hasAttribute(required)){
-    //     alert('please fill in all fields');
-    // }
-    // else {
-        state = {
-            Name: document.querySelector('.name>input').value,
-            Patronym: document.querySelector('.patronym>input').value,
-            Surname: document.querySelector('.surname>input').value,
-            Date: document.querySelector('.date>input').value,
-            University: document.querySelector('.university>input').value,
-            Faculty: document.querySelector('.faculty>input').value,
-            Specialization: document.querySelector('.specialization>input').value,
-            Graduated: document.querySelector('.graduated>input').value,
-            Phone: document.querySelector('.phone>input').value,
-            Email: document.querySelector('.mail>input').value,
-            Skype: document.querySelector('.skype>input').value,
-            Telegram: document.querySelector('.telegram>input').value,
-        };
-        document.querySelector(".name").innerHTML = "Name: " + state.Name;
+    state = {
+        Name: document.querySelector('.name>input').value,
+        Patronym: document.querySelector('.patronym>input').value,
+        Surname: document.querySelector('.surname>input').value,
+        Date: document.querySelector('.date>input').value,
+        University: document.querySelector('.university>input').value,
+        Faculty: document.querySelector('.faculty>input').value,
+        Specialization: document.querySelector('.specialization>input').value,
+        Graduated: document.querySelector('.graduated>input').value,
+        Phone: document.querySelector('.phone>input').value,
+        Email: document.querySelector('.mail>input').value,
+        Skype: document.querySelector('.skype>input').value,
+        Telegram: document.querySelector('.telegram>input').value
+    };
+
+        document.querySelector('.name').innerHTML = "Name: " + state.Name;
         document.querySelector(".patronym").innerHTML = "Patronym: " + state.Patronym;
         document.querySelector(".surname").innerHTML = "Surname: " + state.Surname;
-        document.querySelector(".date").innerHTML = "Date: " + state.Date;
+        document.querySelector(".date").innerHTML = "Birthday: " + state.Date;
         document.querySelector(".university").innerHTML = "University: " + state.University;
         document.querySelector(".faculty").innerHTML = "Faculty: " + state.Faculty;
         document.querySelector(".specialization").innerHTML = "Specialization: " + state.Specialization;
@@ -32,17 +29,37 @@ document.querySelector(".save").addEventListener("click", function(){
         document.querySelector(".mail").innerHTML = "Mail: " + state.Email;
         document.querySelector(".skype").innerHTML = "Skype: " + state.Skype;
         document.querySelector(".telegram").innerHTML = "Telegram: " + state.Telegram;
-        document.querySelector(".main").removeChild(document.querySelector(".save"))
-    // }
+        document.querySelector(".buttons").removeChild(document.querySelector(".save"));
+
+
+        const data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(state));
+        const a = document.createElement("a");
+        a.setAttribute("href", `data: ${data}`);
+        a.setAttribute("download","data.json");
+        a.setAttribute("class","button");
+        a.innerHTML = 'Download page';
+        document.querySelector(".buttons").appendChild(a);
+
 });
 
 document.querySelector(".print").addEventListener("click", function(){
     window.print();
 });
 
-document.querySelector(".attach").addEventListener("click", attachFile)
-function attachFile() {
-    document.getElementById('fileid').click();
-}
+    const uploadButton = document.getElementById("uploadbtn");
+    uploadButton.addEventListener("change", function(){
+        const preview =  document.getElementById("picField");
+        const file    = document.querySelector('input[type=file]').files[0];
+        const reader  = new FileReader();
 
+        reader.onloadend = function () {
+            preview.src = reader.result;
+        };
 
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            preview.src = "";
+        }
+
+});
